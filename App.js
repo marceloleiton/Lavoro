8cb3712ef9dcdd5d82c4b87501b9d44a694f921e
marceloleiton <marceloleiton1997@gmail.com>
2021-04-27 01:11:33 -0400

    dependencias para la navegacion actualizada con imports

--- App.js
+++ App.js
@@ -1,8 +1,6 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import {
-  SafeAreaView,
-  StatusBar,
-} from 'react-native';
+import {SafeAreaView, StatusBar} from 'react-native';
 
 import HomeScreen from './src/screens/Home';
 import Post from './src/components/Post';
@@ -10,20 +8,18 @@ import feed from './assets/data/feed';
 import SearchResultsScreen from './src/screens/SearchResults';
 import DestinationSearchScreen from './src/screens/DestinationSearch';
 
-const post1 = feed[0];
-const post2 = feed[1];
-
 const App: () => React$Node = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView>  
-       {/* <HomeScreen/>*/}
-         {/*<Post post={post1}/>*/}
+      <SafeAreaView>
+        <HomeScreen />
+        {/*<Post post={post1}/>*/}
         {/* <Post post={post2}/>*/}
-       {/* <SearchResultsScreen />*/}
-       <DestinationSearchScreen/>
+        {/* <SearchResultsScreen />*/}
+        {/* <DestinationSearchScreen />*/}
         {/* 2:14:30*/}
+        {/* actual navigation 2:44:30*/}
       </SafeAreaView>
     </>
   );
